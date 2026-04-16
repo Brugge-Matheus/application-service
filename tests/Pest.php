7697e38c3c13848fa5a45685f4847c3d7c36f162
Matheus Brugge <matheusmilczwski@gmail.com>
2026-04-16 17:18:48 -0300

    Feat(Person. Validations) - make tests for personalizated validations

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,5 +5,5 @@ use BruggeMatheus\ServiceLayer\Tests\TestCase;
 
 require_once __DIR__.'/Stubs.php';
 
-uses(TestCase::class)->in('ExecutionTest.php', 'ValidationTest.php', 'Console');
+uses(TestCase::class)->in('ExecutionTest.php', 'ValidationTest.php', 'CustomValidationTest.php', 'Console');
 uses(DatabaseTestCase::class)->in('Database');

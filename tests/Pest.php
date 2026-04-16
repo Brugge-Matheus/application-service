1b5e6b47452225100c3fdb8515ccd970267e99f5
Matheus Brugge <matheusmilczwski@gmail.com>
2026-04-16 15:37:49 -0300

    Feat(Tests) - make tests for simulating artisan command in a laravel project

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,5 +5,5 @@ use BruggeMatheus\ServiceLayer\Tests\TestCase;
 
 require_once __DIR__.'/Stubs.php';
 
-uses(TestCase::class)->in('ExecutionTest.php', 'ValidationTest.php');
+uses(TestCase::class)->in('ExecutionTest.php', 'ValidationTest.php', 'Console');
 uses(DatabaseTestCase::class)->in('Database');
